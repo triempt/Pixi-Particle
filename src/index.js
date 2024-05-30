bf4bf075efb1884287bc70048f5f93bd337d3f19
triempt <triempt@gearinc.com>
2024-05-30 20:25:53 +0700

    adjust revolt

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+(async () => {
 
-const fx = new revolt.FX();
 const app = new PIXI.Application();
+const fx = new revolt.FX();
 
 var width = 1200;
 var height = 640;
 
 await app.init({ backgroundColor: '#000000', width, height });
-document.getElementById('canvas').appendChild(app.canvas);
+// document.getElementById('canvas').appendChild(app.canvas);
+
+console.log("app: ", app);
+console.log("ticker: ", app.ticker);
+var resize = function () {
+    var w = window.innerWidth - 20;
+    var ratio = width / height;
+
+    var h2 = w / ratio;
+    var scale = h2 / height;
+    app.stage.scale.set(scale);
+    app.renderer.resize(w, h2);
+};
+window.addEventListener('resize', resize);
+resize();
+
+var container = new PIXI.Container();
+var debug = new PIXI.Graphics();
+app.stage.addChild(container);
+app.stage.addChild(debug);
+
+
 //Load the assets using PIXI Assets loader
 // PIXI.Assets.add({ alias: 'fx_settings', src: './assets/circle/CircleFX.json' });
 // PIXI.Assets.add({ alias: 'fx_spritesheet', src: './assets/circle/textpacker.json' });
@@ -25,40 +47,58 @@ document.getElementById('canvas').appendChild(app.canvas);
 const rfxBundleSettings = './assets/circle/CircleFX.json';
 const rfxSpritesheet = './assets/circle/textpacker.json';
 const additionalAssets = ['assets/rfx-examples.json'];
-// let sprite = PIXI.Sprite.from('./images/Bubbles99.png');
-// const logo = await PIXI.Sprite.from('./images/Bubbles99.png');
-app.ticker = new PIXI.Ticker();
-console.log("ticker: ", app.ticker);
+
+
 //Load bundle files and the additional example spritesheet
 await fx.loadBundleFiles(rfxBundleSettings, rfxSpritesheet, null, null).then(function (data) {
 
-    var content = new PIXI.Container();
-    content.x = width * 0.5;
-    content.y = height * 0.5;
-    app.stage.addChild(content);
-
-    // var logo = sprite;
+    // var logo = PIXI.Sprite.from('./images/Bubbles99.png');
     // logo.anchor.set(0.5);
     // logo.alpha = 0.6;
     // content.addChild(logo);
 
     const emitter = fx.getParticleEmitter('circle-glow');
+    emitter.init(container);
     emitter.x = 100;
     emitter.y = 100;
     emitter.rotation = Math.PI;
-    emitter.on.started.add(emitter => { console.log("start fx: ", emitter) });
-    emitter.init(content);
+    console.log("start fx: ", emitter) ;
+    emitter.on.started.add(emitter => {});
+    //Register for a particle spawned signal (event)
+    emitter.on.particleSpawned.add(function (particle) {
+        console.log('particle: ', particle);
 
+        drawDot(particle.x, particle.y, 20, 0x00ff00);
 
-    app.ticker.add(function () {
-        //Update the RevoltFX instance
-        fx.update();
+        //Register for an update signal for that particle
+        particle.on.updated.add(function (particle) {
+            drawDot(particle.x, particle.y, 5, 0x00ff00);
+        });
+
+        //Register for a died signal for that particle
+        particle.on.died.add(function (particle) {
+            drawDot(particle.x, particle.y, 15, 0xff0000);
+        });
     });
+
+    emitter.on.completed.addOnce(function (emitter) {
+        console.log('Done');
+    });
+
+
 
 }).catch(function (err) {
     console.log('Error', err);
 });
 
+app.ticker.add(function (delta) {
+    //Update the RevoltFX instance
+    fx.update(delta);
+});
 
+function drawDot(x, y, size, color) {
+    debug.beginFill(color, 0.2).drawCircle(x, y, size).endFill();
+}
 //Inititialize it with the target PIXI container
 
+})();
